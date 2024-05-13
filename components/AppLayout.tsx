75d9b50cbf31ef09b0dbfc018ec6e4e473566128
aLANparty <aLANparty@protonmail.com>
2024-05-13 09:16:33 -0400

    notification permission

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,11 +1,35 @@
 import { ConnectWallet } from '@/components/ConnectWallet'
 import { ROUTES } from '@/utils/constants'
 import { Box, Heading, HStack, Spacer } from '@chakra-ui/layout'
+import { Button } from '@chakra-ui/button'
 import { Link as NextLink } from '@chakra-ui/next-js'
+import { useState, useEffect } from 'react'
+
 export const AppLayout = ({ children }) => {
+  const [notificationsAllowed, setNotificationsAllowed] = useState(false)
+
+  function requestPermissions() {
+    Notification.requestPermission().then((permission) => {
+      console.log(permission)
+      setNotificationsAllowed(permission === 'granted' ? true : false)
+    })
+  }
+
+  useEffect(() => {
+    requestPermissions()
+  }, [])
+
   return (
     <div>
       <header>
+        {!notificationsAllowed && (
+          <Box textAlign="center" bg="#e5e5e5" p={2}>
+            Notifications are not enabled.{' '}
+            <Button onClick={requestPermissions} size="sm">
+              Enable
+            </Button>
+          </Box>
+        )}
         <HStack p="4" borderBottom="1px" boxShadow="lg" alignItems="center">
           <NextLink href="/" display="block">
             <Heading
