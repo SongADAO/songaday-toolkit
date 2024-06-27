a7ececef3dc89a6cdd5651e70335205da0468034
aLANparty <aLANparty@protonmail.com>
2024-06-27 12:25:18 -0400

    remove unused imports

--- pages/bulk/index.tsx
+++ pages/bulk/index.tsx
@@ -1,7 +1,7 @@
 import { AppLayout } from '@/components/AppLayout'
 import { Card } from '@/components/Card'
 import { BULK_ROUTES } from '@/utils/constants'
-import { Heading, Link, Stack, Text } from '@chakra-ui/react'
+import { Heading, Stack, Text } from '@chakra-ui/react'
 import { Link as NextLink } from '@chakra-ui/next-js'
 
 export default function Bulk() {
